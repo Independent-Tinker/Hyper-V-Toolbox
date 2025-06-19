787a91f025734b7817bf6cd6ab6af202d4e67919
Independent <gpdally@outlook.com>
2025-06-19 14:30:08 +0200

    Add script to configure APT sources for Debian 12 (bookworm)

---
+++ client-tools/repo-list-debian12.sh
@@ -0,0 +1,13 @@
+tee /etc/apt/sources.list<<EOF
+deb http://deb.debian.org/debian bookworm main contrib non-free-firmware
+# deb-src http://deb.debian.org/debian bookworm main contrib non-free-firmware
+
+deb http://deb.debian.org/debian bookworm-updates main contrib non-free-firmware
+# deb-src http://deb.debian.org/debian bookworm-updates main contrib non-free-firmware
+
+# deb http://deb.debian.org/debian bookworm-backports main contrib non-free-firmware
+# deb-src http://deb.debian.org/debian bookworm-backports main contrib non-free-firmware
+
+deb http://security.debian.org/debian-security bookworm-security main contrib non-free-firmware
+# deb-src http://security.debian.org/debian-security bookworm-security main contrib non-free-firmware
+EOF
